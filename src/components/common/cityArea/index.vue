321ae13f2e5dda642974d2bfa7388aac146c8bb3
yfzan <yfzan@clustertech.com>
2020-10-21 17:54:14 +0800

    组件

---
+++ src/components/common/cityArea/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-cascader v-model="value" :props="cascader_props" @change="changeValue"></el-cascader>
+</template>
+
+<script>
+import { GetCity } from "@/api/common";
+export default {
+  name:"CityArea",
+  props:{
+    cityAreaValue:{
+      type:String,
+      default:""
+    }
+  },
+  data(){
+    return{
+      value:"",
+      cascader_props:{
+        lazy: true,
+        lazyLoad (node, resolve) {
+          const level = node.level;
+          const requestData = {};
+          // 声明自定义配置
+          const jsonType = {
+            0: { type: "province" },
+            1: { type: "city", code: "province" },
+            2: { type: "area", code: "city" }
+          }
+          if(jsonType[level].code) { requestData[`${jsonType[level].code}_code`] = node.value }
+          // type
+          requestData.type = jsonType[level].type;
+          GetCity(requestData).then(response=>{
+            const data = response.data.data;
+            const type = jsonType[level].type.toUpperCase();
+            data.forEach(item => {
+              item.value = item[`${type}_CODE`];
+              item.label = item[`${type}_NAME`];
+              // 最后一层选择
+              if(level === 2) { item.leaf = level >= 2; }
+            })
+            resolve(data);
+          })
+        }
+      }
+    }
+  },
+  methods:{
+    changeValue(value){
+      this.$emit("update:cityAreaValue",value.join());
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/dialog/addCarsBrand.vue
@@ -0,0 +1,74 @@
+<template>
+  <el-dialog 
+  title="新增" 
+  class="cars-dialog-center" 
+  :visible.sync="dialogVisible" 
+  :close-on-click-modal="false"
+  @close="close"
+  >
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="车辆品牌">
+        <el-input v-model="form.user"></el-input>
+      </el-form-item>
+      <el-form-item label="品牌型号">
+        <el-input v-model="form.user"></el-input>
+      </el-form-item>
+      <el-form-item label="LOGO">
+        <div class="upload-img-wrap">
+          <div class="upload-img"></div>
+          <div class="img-list"></div>
+        </div>
+      </el-form-item>
+      <el-form-item label="禁启用">
+        <el-radio-group v-model="form.user">
+          <el-radio label="禁用"></el-radio>
+          <el-radio label="启用"></el-radio>
+        </el-radio-group>
+      </el-form-item>           
+      <el-form-item label="描述">
+        <el-input type="textarea" v-model="form.user"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="close">取 消</el-button>
+      <el-button type="primary" @click="close">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name:"AddCarsBrand",
+  props:{
+    flagVisible:{
+      type:Boolean,
+      default:false
+    }
+  },
+  data(){
+    return{
+      dialogVisible:false,
+      form: {
+        user: '',
+        region: ''
+      },
+    }
+  },
+  methods:{
+    close(){
+      this.$emit("update:flagVisible",false);
+    }
+  },
+  watch:{
+    flagVisible:{
+      handler(newValue,oldValue){
+        this.dialogVisible = newValue;
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
