b72ee7113a4a7a5f4d6cc380db07eddd6b100147
yfzan <yfzan@clustertech.com>
2020-10-22 17:48:52 +0800

    地图组件增加功能

--- src/views/amap/index.vue
+++ src/views/amap/index.vue
@@ -6,22 +6,52 @@
 
 <script>
 import { AMapManager,lazyAMapApiLoaderInstance } from 'vue-amap';
+import { getLngLat } from "./common";
+import { addressSetMapCenter } from "./location";
+import { amapSetMarker,amapClearMarker } from "./marker";
 export default {
   name:"Map",
   data(){
     return{
+      lnglat:{},
       map:null,
       zoom:18,
       events:{}
     }
   },
   mounted(){
+    const _this = this;
     lazyAMapApiLoaderInstance.load().then(() => {
       this.map = new AMap.Map('amapContainer', {
         zoom:this.zoom,//级别
         center: [116.397428, 39.90923],//中心点坐标
       });
+      this.map.on("click",function(e){//function会改变this指向
+        const lnglat = getLngLat(e);
+        _this.lnglat = lnglat;
+        _this.$emit("callback",{
+          function:"getLngLatValue",
+          data:{
+            lnglat
+          }
+        })
+        _this.setMarker();
+      })
     })
+  },
+  methods:{
+    /** 根据选择区域设置地图中心点 */
+    setMapCenter(address){
+      addressSetMapCenter(address,this.map);
+    },
+    /** 设置点覆盖物 */
+    setMarker(){
+      amapSetMarker(this.lnglat,this.map);
+    },
+    /** 清除点覆盖物 */
+    clearMarker(){
+      amapClearMarker(this.map);
+    }
   }
 }
 </script>
---
+++ src/views/amap/marker.js
@@ -0,0 +1,15 @@
+let marker=null;
+export function amapSetMarker(lnglat,map){
+  if(marker){
+    map.remove(marker);
+  }
+  marker = new AMap.Marker();
+  marker.setPosition([lnglat.lng,lnglat.lat]);
+  map.add(marker);
+}
+export function amapClearMarker(map){
+  if(marker) {
+      map.remove(marker);
+      marker = null;
+  }
+}
