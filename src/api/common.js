0d61f334581d24e61933c70aee6db6e2ab26fc6a
yfzan <yfzan@clustertech.com>
2020-10-28 13:46:56 +0800

    公共接口

--- src/api/common.js
+++ src/api/common.js
@@ -53,3 +53,14 @@ export function GetParking(data = {}){
       data
   })
 }
+
+/**
+ * 获取七牛云 token
+ */
+export function GetQiniuToken(data = {}){
+  return service.request({
+    method: "post",
+    url: "/uploadImgToken/",
+    data
+  })
+}
