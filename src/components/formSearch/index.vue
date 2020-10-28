76a39af9de6af721b680d8260650e87cf93d2c09
yfzan <yfzan@clustertech.com>
2020-10-28 13:46:15 +0800

    表格数据搜索组件

--- src/components/formSearch/index.vue
+++ src/components/formSearch/index.vue
@@ -1,59 +1,72 @@
 <template>
-  <el-form :inline="true" class="demo-form-inline" :model="form_data">
-    <el-form-item v-for="item in formItem" :label="item.label" :prop="item.prop" :key="item.prop" :rules="item.rules">
-      <!-- 输入框 -->
-      <el-input 
-        v-if="item.type === 'Input'" 
-        v-model.trim="form_data[item.prop]" 
-        :placeholder="item.placeholder" 
-        :style="{ width:item.width }"       
-        :readonly="item.readonly" 
-        :disabled="item.disabled"
-      ></el-input>
-      <!-- 选择器 -->
-      <el-select v-if="item.type === 'Select'" v-model="form_data[item.prop]" clearable filterable :placeholder="item.placeholder" :style="{ width:item.width }" >
-        <el-option
-          v-for="selectItem in item.options"
-          :key="selectItem.value !== undefined ?  selectItem.value : selectItem[item.select_value]"
-          :label="selectItem.label || selectItem[item.select_label]"
-          :value="selectItem.value !== undefined ?  selectItem.value : selectItem[item.select_value]">
-        </el-option>
-      </el-select>
-      <!-- 单选框 -->
-      <el-radio-group v-if="item.type === 'Radio'" v-model="form_data[item.prop]" >
-        <el-radio 
-          v-for="radio in item.options" 
-          :label="radio.value" 
-          :key="radio.value"
-        >
-          {{ radio.label }}
-        </el-radio>
-      </el-radio-group>
-      <!-- 计数器 -->
-      <el-input-number v-if="item.type === 'InputNumber'" v-model.trim="form_data[item.prop]" :min="item.min" :max="item.max"></el-input-number>    
-      <!-- 省市区 -->
-      <template v-if="item.type === 'City'">
-        <CityArea :cityAreaValue.sync="form_data[item.prop]" />
-      </template>
-      <!-- 关键字 -->
-      <template v-if="item.type === 'KeyWord'">
-        <KeyWord ref="keyword" :options="['parkingName','address']" :value.sync="keyword" />
-       </template>
-      <!-- 插槽 -->
-      <slot v-if="item.type === 'Slot'" :name="item.slotName"></slot>    
-    </el-form-item>
-    <el-form-item>
-      <el-button type="danger" @click="search">搜索</el-button>
-      <el-button 
-        v-for="item in formHandler" 
-        :key="item.key" 
-        :type="item.type" 
-        @click="item.handler && item.handler()"
-      >
-        {{ item.label }}
-      </el-button>
-    </el-form-item>
-  </el-form>
+  <div class="filter-form">
+    <el-row>
+      <el-col :span="formConfig.formCol">
+        <el-form :inline="true" class="demo-form-inline" :model="form_data" ref="form">
+          <el-form-item v-for="item in formItem" :label="item.label" :prop="item.prop" :key="item.prop" :rules="item.rules">
+            <!-- 输入框 -->
+            <el-input 
+              v-if="item.type === 'Input'" 
+              v-model.trim="form_data[item.prop]" 
+              :placeholder="item.placeholder" 
+              :style="{ width:item.width }"       
+              :readonly="item.readonly" 
+              :disabled="item.disabled"
+            ></el-input>
+            <!-- 选择器 -->
+            <el-select v-if="item.type === 'Select'" v-model="form_data[item.prop]" clearable filterable :placeholder="item.placeholder" :style="{ width:item.width }" >
+              <el-option
+                v-for="selectItem in item.options"
+                :key="selectItem.value !== undefined ?  selectItem.value : selectItem[item.select_value]"
+                :label="selectItem.label || selectItem[item.select_label]"
+                :value="selectItem.value !== undefined ?  selectItem.value : selectItem[item.select_value]">
+              </el-option>
+            </el-select>
+            <!-- 单选框 -->
+            <el-radio-group v-if="item.type === 'Radio'" v-model="form_data[item.prop]" >
+              <el-radio 
+                v-for="radio in item.options" 
+                :label="radio.value" 
+                :key="radio.value"
+              >
+                {{ radio.label }}
+              </el-radio>
+            </el-radio-group>
+            <!-- 计数器 -->
+            <el-input-number v-if="item.type === 'InputNumber'" v-model.trim="form_data[item.prop]" :min="item.min" :max="item.max"></el-input-number>    
+            <!-- 省市区 -->
+            <template v-if="item.type === 'City'">
+              <CityArea ref="city" :cityAreaValue.sync="form_data[item.prop]" />
+            </template>
+            <!-- 关键字 -->
+            <template v-if="item.type === 'KeyWord'">
+              <KeyWord ref="keyword" :options="item.options" :value.sync="keyword" />
+            </template>
+            <!-- 插槽 -->
+            <slot v-if="item.type === 'Slot'" :name="item.slotName"></slot>    
+          </el-form-item>
+          <el-form-item>
+            <el-button type="danger" @click="search">搜索</el-button>
+            <el-button @click="reset" v-if="formConfig.resetButton">重置</el-button>             
+          </el-form-item>
+        </el-form>
+      </el-col>
+      <el-col :span="formConfig.handlerCol">
+        <div class="pull-right">
+          <template v-for="item in formHandler">
+            <el-button v-if="item.element === 'link'" :key="item.key" :type="item.type">
+              <router-link :to="item.router" >
+                <span class="color-white">{{ item.label }}</span>
+              </router-link>
+            </el-button>
+            <el-button v-if="item.element === 'button'" :key="item.key" :type="item.type" @click="item.handler && item.handler()">
+              {{ item.label }}
+            </el-button>
+          </template>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -69,6 +82,10 @@ export default {
     formHandler:{
       type:Array,
       default:() => []
+    },
+    formConfig:{
+      type: Object,
+      default: () => {}
     }
   },
   components:{ CityArea,KeyWord },
@@ -121,8 +138,10 @@ export default {
         data.options = options;
       }
     },
-    resetForm(){
+    reset(){
       this.$refs.form.resetFields();     
+      if(this.$refs.city && this.$refs.city[0]) this.$refs.city[0].clear();
+      if(this.$refs.keyword && this.$refs.keyword[0]) this.$refs.keyword[0].clear();
     }
   },
   watch:{
